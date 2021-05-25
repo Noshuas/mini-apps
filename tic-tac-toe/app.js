cc4a9e1769619c72b3c129561efe5bb71128b7c6
Noshua <Noshuas@gmail.com>
2021-05-24 20:34:07 -0700

    Completed all of the basic requirements and most of the advanced content.

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const app = express();
-const mysql = require('mysql');
-const Promise = require('bluebird');
-const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'ticTacToe'
-});
+// const express = require('express');
+// const app = express();
+// const mysql = require('mysql');
+// const Promise = require('bluebird');
+// const con = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'ticTacToe'
+// });
+// debugger;
 
 /*
         ::Models
@@ -15,7 +16,7 @@ const con = mysql.createConnection({
 
 
 class Board {
-  constuctor(){
+  constructor(){
     this.state = {
       row0: 0,
       row1: 0,
@@ -25,121 +26,224 @@ class Board {
       col2: 0,
       leadDiagonal: 0,
       antiDiagonal: 0,
-      clear: ()=>{
-        Object.keys(this).forEach(key => this.key = 0);
-      }
+      turn: 0
     }
+    this.currentPlayer = 'x';
+    this.history = [];
   }
 
-  updateState(str) {
-    var incrementer = (str[0] * 1) ? -1 : 1;
-    var row = str[1];
-    var col = str[2];
+  updateState(target) {
+    view.addSymbol(target);
+
+    var rowCol = target.classList[1];
+    var player = game.currentPlayer;
+    var incrementer = (player === 'o') ? -1 : 1;
+    var row = rowCol[0];
+    var col = rowCol[1];
+
     this.state['row' + row] += incrementer;
     this.state['col' + col] += incrementer;
-    if (row === 1 && col === 1) {
-      this.leadDiagonal += incrementer;
-      this.antiDiagonal += incrementer;
+    if (row * 1 === 1 && col * 1 === 1) {
+      this.state.leadDiagonal += incrementer;
+      this.state.antiDiagonal += incrementer;
     } else {
-      if (row === col) this.leadDiagonal += incrementer;
-      if (Math.abs(row - col) === 2 ) this.antiDiagonal += incrementer;
+      if (row === col) this.state.leadDiagonal += incrementer;
+      if (Math.abs(row - col) === 2 ) this.state.antiDiagonal += incrementer;
+    }
+    this.state.turn++;
+    this.checkForWin(player);
+  }
+
+  // newMove(str) {
+  //   con.connect((err)=>{
+  //     if (err) throw err;
+  //     var query = 'insert into turns (turn) value ?'
+  //     con.query(query, [[str]], (err, results)=>{
+  //       if (err) throw err;
+  //       this.updateState(str);
+  //       //call a view function here
+  //       console.log(results);
+  //       con.end();
+  //     });
+  //   });
+  // }
+
+  // restart() {
+  //   con.connect((err)=>{
+  //     if (err) throw err;
+  //     con.query('delete from turns', (err, results)=>{
+  //       if (err) throw err;
+  //       Object.keys(this.state).forEach(key => this.state.key = 0);
+  //       //call a view function here
+  //       con.end();
+  //     });
+  //   });
+  // }
+
+  // initState(){
+  //   con.connect(err=>{
+  //     if (err) throw err;
+  //     con.query('select changes from turns', (err, results) => {
+  //       if (err) throw err;
+  //       if (changes) {
+  //         results.forEach(result=>{
+  //           this.updateState(result.changes);
+  //         });
+  //         console.log('Board state loaded');
+  //       } else {
+  //         console.log('Board state already empty');
+  //       }
+  //       con.end();
+  //     });
+  //   });
+  // }
+
+  reset() {
+    Object.keys(this.state).forEach(key => this.state[key] = 0);
+    view.clearBoard();
+  }
+
+  checkForWin(player) {
+    var gameover, winner;
+    var goal = (player === 'x') ? 3 : -3;
+
+    Object.keys(this.state).forEach(key=>{
+      if (this.state[key] === goal && key !== 'turn') {
+        gameover = true;
+        winner = player;
+      } else if (this.state.turn === 9) {
+        gameover = true;
+        winner = 't';
+      }
+    });
+    this.currentPlayer = (this.currentPlayer === 'x') ? 'o' : 'x';
+    if (gameover) {
+      if (winner !== 't') this.currentPlayer = winner;
+      this.reset();
+      view.displayWinner(winner);
     }
   }
-
-  newMove(str) {
-    con.connect((err)=>{
-      if (err) throw err;
-      var query = 'insert into turns (turn) value ?'
-      con.query(query, [[str]], (err, results)=>{
-        if (err) throw err;
-        this.updateState(str);
-        //call a view function here
-        console.log(results);
-        con.end();
-      });
-    });
-  }
-
-  restart() {
-    con.connect((err)=>{
-      if (err) throw err;
-      con.query('delete from turns', (err, results)=>{
-        if (err) throw err;
-        this.state.clear();
-        //call a view function here
-        con.end();
-      });
-    });
-  }
-
-  initState(){
-    con.connect(err=>{
-      if (err) throw err;
-      con.query('select changes from turns', (err, results) => {
-        if (err) throw err;
-        if (changes) {
-          results.forEach(result=>{
-            this.updateState(result.changes);
-          });
-          console.log('Board state loaded');
-        } else {
-          console.log('Board state already empty');
-        }
-        con.end();
-      });
-    });
-  }
 }
 
-class Games {
-  constuctor(){
-  }
+// class Game {
+//   constructor(player = 'x', prevGames = []){
+//     this.currentPlayer = player;
+//     this.history = prevGames;
+//   }
 
-  getAll(callback) {
-    con.connect(err => {
-      if (err) throw err;
-      con.query('select * from games', (err, results)=>{
-        if (err) throw err;
-        if (results) {
-          results.forEach(result=>{
-            callback(null, result);
-          })
-        }
-        con.end();
-      });
-    });
-  }
+  // getAll(callback) {
+  //   con.connect(err => {
+  //     if (err) throw err;
+  //     con.query('select * from games', (err, results)=>{
+  //       if (err) throw err;
+  //       if (results) {
+  //         results.forEach(result=>{
+  //           callback(null, result);
+  //         })
+  //       }
+  //       con.end();
+  //     });
+  //   });
+  // }
 
-  addResult(str) {
-    con.connect(err => {
-      if (err) throw err;
-      var query = 'insert into games (winner) values ?'
-      con.query(query, [[str]], (err, results)=>{
-        if (err) throw err;
-        console.log('winner added to table');
-        con.end();
-      });
-    });
-  }
+  // addResult(str) {
+  //   con.connect(err => {
+  //     if (err) throw err;
+  //     var query = 'insert into games (winner) values ?'
+  //     con.query(query, [[str]], (err, results)=>{
+  //       if (err) throw err;
+  //       console.log('winner added to table');
+  //       con.end();
+  //     });
+  //   });
+  // }
 
-}
+// }
+
+
 
 /*
         ::Views
         __________________________
 */
 
+class View {
+  constructor(){
+
+  }
+
+  addSymbol(target) {
+    target.innerHTML = game.currentPlayer;
+  }
+
+  clearBoard() {
+    var spaces = document.getElementsByClassName('space');
+    for (var i = 0; i < spaces.length; i++) {
+      var space = spaces[i];
+      space.innerHTML = '';
+    }
+  }
+
+  displayWinner(winner) {
+    var winMessage = "The game was a tie";
+    if (winner === 'x') {
+      winMessage = "Player 1 (x) has won the game";
+    } else if (winner === 'o') {
+      winMessage = "Player 2 (o) has won the game";
+    }
+
+    var middleSquare = document.getElementsByClassName('11')[0];
+    setTimeout(()=>{middleSquare.innerHTML = ''}, 2000);
+    middleSquare.innerHTML = winMessage;
+
+    game.history.push(winMessage);
+    var history = document.getElementsByClassName('hist')[0];
+    var p = document.createElement('p');
+    p.innerHTML = winMessage;
+    history.prepend(p)
+  }
+}
 /*
         ::Controllers
         __________________________
 */
+class Controller {
+  constructor(){
+     this.spaces = document.getElementsByClassName('space');
+     this.resetButton = document.getElementById('reset');
+
+     for (var i = 0; i < this.spaces.length; i++) {
+      var space = this.spaces[i];
+      space.addEventListener('click', this.playerMove);
+    }
+
+    this.resetButton.addEventListener('click', this.reset);
+  }
+
+  playerMove = function (e) {
+    var target = e.target;
+    game.updateState(target);
+  }
+
+  reset(){
+    game.reset();
+    game.currentPlayer = 'x';
+  }
+}
+
+var game = new Board();
+var view = new View();
+var controller = new Controller();
+
+// spaces.addEventListener('click', getClicked);
+
 /*
         ::Routes
         __________________________
 */
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 
 
@@ -163,6 +267,6 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(3000, ()=>
-  console.log('Example app listening at http://localhost:3000')
-);
+// app.listen(3000, ()=>
+//   console.log('Example app listening at http://localhost:3000')
+// );
